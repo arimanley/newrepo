be1077e34bc44240f5e60e0c032e8fb0b96031be
arimanley <156138768+arimanley@users.noreply.github.com>
2024-02-07 11:30:38 -0600

    ready

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -233,3 +233,23 @@ VALUES   (
     'White',
     5
   );
+
+--Modify the "GM Hummer" record to read "a huge interior" rather than "small interiors" using a single query.
+UPDATE inventory
+SET inv_description = REPLACE(
+inv_description,
+'small interiors',
+'huge interiors'
+)
+WHERE inv_model='Hummer';
+
+--Update all records in the inventory table to add "/vehicles" to the middle of the file path in the inv_image and inv_thumbnail columns using a single query.
+UPDATE inventory
+SET inv_image=REPLACE(
+inv_image,
+'/images/',
+'/images/vehicles/'),
+inv_thumbnail= REPLACE(
+inv_thumbnail,
+'/images/',
+'/images/vehicles/');
